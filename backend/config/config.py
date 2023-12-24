15559c2c7a8755dc51d510f6259c4642996aab15
brian861105 <brian861105@gmail.com>
2023-12-24 13:15:02 +0000

    feat: 🎸 update backend-test

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,3 +1,3 @@
-MONGODB_KEY = "WyJRepqRLIZ7K5CJ"
-MONGODB_URI = f"mongodb+srv://master:{MONGODB_KEY}@cluster0.7pgqvs4.mongodb.net/?retryWrites=true&w=majority"
-OPENAI_KEY = "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"
+MongoKey = "WyJRepqRLIZ7K5CJ"
+MongoURI = f"mongodb+srv://master:{MongoKey}@cluster0.7pgqvs4.mongodb.net/?retryWrites=true&w=majority"
+OpenaiKey = "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"
--- backend/test/test_Database.py
+++ backend/test/test_Database.py
@@ -1,46 +1,74 @@
-# test_database.py
 import pytest
 from pymongo import MongoClient
-from config.config import MONGODB_URI
+from config.config import MongoURI
 from src.Database import DatabaseManager
+import json
 
+
+def is_valid_json(json_str):
+    try:
+        json.loads(json_str)
+        return True
+    except json.JSONDecodeError:
+        return False
+
+
+### With MockDB
 @pytest.fixture
 def mongo_client():
-    client = MongoClient(MONGODB_URI)
+    client = MongoClient(MongoURI)
     yield client
     client.close()
 
-def test_connect_to_mongodb(mongo_client):
-    db = mongo_client.get_database('my_database')
+
+def test_ConnectMongodb(mongo_client):
+    db = mongo_client.get_database("my_database")
     assert mongo_client.is_primary
 
-def test_database_manager_with_mock():
-    database_manager = DatabaseManager(mock_db=True, simulate_connection_failure=False)
-    assert database_manager.is_connect() == True
 
-def test_database_manager_with_mock_fail():
+def test_CreateUserDatabaseWithMock():
+    database_manager = DatabaseManager()
+    UserId = "UserId"
+    assert database_manager.CreateUser(UserId) == True
 
-    database_manager_failure = DatabaseManager(mock_db=True, simulate_connection_failure=True)
-    assert database_manager_failure.is_connect() == False
 
-def test_create_database():
-    database_manager = DatabaseManager(mock_db=True, simulate_connection_failure=False)
-    collection_name = "test_collection"
-    assert database_manager.create_collection(collection_name) == True
+def test_ReadUserChatSession():
+    database_manager = DatabaseManager()
+    UserId = "UserId"
+    assert is_valid_json(database_manager.ReadUser(UserId)) == True
 
-# def test_create_collection_already_exists():
-#     database_manager = DatabaseManager(mock_db=True, simulate_connection_failure=False)
-#     assert database_manager.is_connect() == True
 
-#     collection_name = "test_collection"
-#     assert database_manager.create_collection(collection_name) == True
+def test_DeleteUserDatabase():
+    database_manager = DatabaseManager()
+    UserId = "UserId"
+    assert database_manager.DeleteUser(UserId) == True
 
-#     assert database_manager.create_collection(collection_name) == False
-# def test_read_database():
 
-# def test_update_database():
+def test_CreateSessionDatabase():
+    database_manager = DatabaseManager()
+    UserId = "UserId"
+    ResultData = database_manager.CreateSession(UserId)
+    assert is_valid_json(ResultData) == True
+    ResultData = json.loads(ResultData)
+    assert isinstance(ResultData["SessionId"], str) == True
 
-# def test_delete_database():
 
-# def test_database_is_primary():
-#     database_manager = DatabaseManager(mock_db=False)
+def test_DeleteSessionDatabase():
+    database_manager = DatabaseManager()
+    UserId = "UserId"
+    CreateData = database_manager.CreateSession(UserId)
+    CreateData = json.loads(CreateData)
+    SessionId = CreateData["SessionId"]
+    result = database_manager.DeleteSession(UserId, SessionId)
+    assert result == True
+
+
+# def test_RenameSessionTitle():
+#     database_manager = DatabaseManager()
+#     UserId = "UserId"
+#     CreateData = database_manager.CreateSession(UserId)
+#     CreateData = json.loads(CreateData)
+#     SessionId = CreateData["SessionId"]
+#     NewTitle = "hello world"
+#     result = database_manager.RenameSessionTitle(UserId, SessionId, NewTitle)
+#     assert result == True
