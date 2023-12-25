215c1399b999539544fdc10308a58913992efc7a
YOU CHEN <brian861105@gmail.com>
2023-12-25 23:29:26 +0800

    test: 💍 remove config

--- backend/config/config.py
+++
@@ -1,3 +0,0 @@
-MONGODB_KEY = "WyJRepqRLIZ7K5CJ"
-MONGODB_URI = f"mongodb+srv://master:{MONGODB_KEY}@cluster0.7pgqvs4.mongodb.net/?retryWrites=true&w=majority"
-OPENAI_KEY = "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"
