8a811ea56b399c7bd16eeae8b427c9b884ee82d0
YOU CHEN <brian861105@gmail.com>
2023-12-25 23:39:38 +0800

    fix: 🐛 remove config

--- backend/config/config.py
+++
@@ -1,3 +0,0 @@
-MongoKey = "WyJRepqRLIZ7K5CJ"
-MongoURI = f"mongodb+srv://master:{MongoKey}@cluster0.7pgqvs4.mongodb.net/?retryWrites=true&w=majority"
-OpenaiKey = "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"
