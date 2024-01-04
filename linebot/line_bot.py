7df6c2910127a49b7ae29838799deb3a438fa5cc
brian861105 <brian861105@gmail.com>
2024-01-04 07:08:24 +0000

    feat: 🎸 update linebot

--- linebot/line_bot.py
+++ linebot/line_bot.py
@@ -2,29 +2,25 @@ import os
 
 channel_secret = os.getenv('CHANNEL_SECRET')
 channel_access_token = os.getenv('CHANNEL_ACCESS_TOKEN')
-
+target_api_url = os.getenv('FLASK_API_URL')
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import requests
 
 app = Flask(__name__)
 
-# 設定 Channel Access Token 與 Channel Secret
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # signature = request.headers['X-Line-Signature']
-    # body = request.get_data(as_text=True)
-
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
     try:
-        data = request.json
-        # print(data)
-        print(data["events"][0]["message"]["text"])
-        # handler.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
@@ -33,9 +29,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message_text = event.message.text
-    reply_text = f"You said: {message_text}"
+    payload = {'text', message_text}
+    response = requests.post(target_api_url, json=payload)
     line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text=reply_text))
+                               TextSendMessage(text=response))
+
+
+@handler.default()
+def default(event):
+    line_bot_api.reply_message(event.reply_token,
+                               TextSendMessage(text="請使用文字訊息回覆，謝謝！"))
 
 
 if __name__ == "__main__":
